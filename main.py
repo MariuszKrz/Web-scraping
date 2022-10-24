c9179b652cadd066f86e5920808aa9d28a575b0c
MariuszKrz <krzystoszek78@gmail.com>
2022-10-24 07:02:52 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/bydgoszcz?distanceRadius=50&market=ALL&locations=%5Bcities_6-184%5D&viewType=listing&lang=pl&searchingCriteria=sprzedaz&searchingCriteria=mieszkanie&searchingCriteria=cala-polska'
+page = requests.get(url)
+
+bs = BeautifulSoup(page.content, 'html.parser')
+
+for offer in bs.find_all('li'):
+    print(offer)
